e837b08e4a3ee93d828e7caabe78594224940672
Katerina Zhadko <katerinkaj@icloud.com>
2021-08-17 00:31:48 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    if (number === 0) { return 'zero';}
+
+    const toNinety = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
+        "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+
+    if (number < 20) {
+        return toNinety[number];
+    }
+
+    const fromTwenty = [ "", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+
+    let calc;
+    let fullNumber;
+
+    if (number < 100) {
+        calc = Math.floor(number / 10);
+        fullNumber = (fromTwenty[calc] + " " + toNinety[number % 10]);
+    }
+    else {
+        calc = Math.floor(number / 100);
+        const dozens = Math.floor(number % 100);
+        const ones = Math.floor(dozens / 10);
+        if (dozens >= 20) {
+            fullNumber = toNinety[calc] + ' hundred ' + fromTwenty[ones] + " " + toNinety[dozens % 10]
+        }
+        else {
+            fullNumber = toNinety[calc] + ' hundred ' + toNinety[dozens];
+        }
+    }
+    return fullNumber.trim();
+};
